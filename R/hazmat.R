ac2c3d6f50b6dabb7910c504b7605d4e0850280e
Ryan Thomas <rthomas2003@gmail.com>
2019-10-27 14:42:18 -0400

    added scan_file and refactored identify_hazard so that emoji logic did not need to be duplicated

--- R/hazmat.R
+++ R/hazmat.R
@@ -3,10 +3,11 @@
 #' @param lines a character vector of to check for hazards
 #' @param pattern a regexp to identify the hazard
 #' @param color a function to highlight the hazard within a line of text
+#' @param emoji an emoji to include in the the output
 #'
 #' @return a tibble with lines of text which match the hazard along with the
 #' corresponding line numbers
-identify_hazard <- function(lines, pattern, color = function(x) x){
+identify_hazard <- function(lines, pattern, color = function(x) x, emoji = ""){
     if (!is.function(color)) {
         stop("`color` must be a fuction. It should modify its string argument",
              call. = FALSE)
@@ -22,22 +23,58 @@ identify_hazard <- function(lines, pattern, color = function(x) x){
                                              replacement = color("\\1"))
 
     line_nums <- seq_along(lines)[idx]
-
-    tibble::tibble(line = line_nums, text = marked_lines[idx])
+    if (length(line_nums) == 0L) {
+        emoji <- character(0)
+    }
+    tibble::tibble(line = line_nums, text = marked_lines[idx], emoji = emoji)
 }
 
 identify_rm <- function(lines) {
     pattern <- "rm\\(ls = ls\\(\\)\\)"
-    identify_hazard(lines, pattern, color = crayon::red)
+    emoji <- intToUtf8(0x1f525) #  Fire Emoji
+    identify_hazard(lines, pattern, color = crayon::red, emoji = emoji)
 }
 
 identify_setwd <- function(lines) {
     pattern <- "setwd\\([^\\)]+\\)"
-    identify_hazard(lines, pattern, color = crayon::red)
+    emoji <- intToUtf8(0x1f525) #  Fire Emoji
+    identify_hazard(lines, pattern, color = crayon::red, emoji = emoji)
 }
 
 identify_system <- function(lines) {
     pattern <- "system(2)?\\('[^\\')]+'\\)"
-    identify_hazard(lines, pattern, color = crayon::yellow)
+    emoji <- intToUtf8(0x1f514) #  Bell Emoji
+    identify_hazard(lines, pattern, color = crayon::yellow, emoji = emoji)
 }
 
+screen_file <- function(path = NULL, quiet = FALSE){
+    if (is.null(path)) {
+        stop("`path` should be a valid file path.", call. = FALSE)
+    }
+
+    if (fs::file_access(path, mode = "read")) {
+        stop("Cannot read `path`. ",
+             "You may not have read access or the file may not exist.",
+             call. = FALSE)
+    }
+    file_lines <- readLines(path)
+
+    hazards <- dplyr::bind_rows(
+        identify_rm(file_lines),
+        identify_setwd(file_lines),
+        identify_system(file_lines)
+    )
+
+    hazards <- dplyr::arrange_at(hazards, .vars = "line")
+    if (!quiet) {
+
+        output <- glue::glue_data(hazards, "{emoji} {line}: {text}")
+        purrr::walk(output, usethis::ui_line)
+    }
+
+    invisible(hazards)
+}
+
+format_hazards <- function(hazards) {
+    glue::glue_data(hazards, "{emoji} {line}: {text}")
+}
