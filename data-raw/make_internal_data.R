f4d3aa8b4291f64e238b0563d15dd7fe43027010
Ryan Thomas <rthomas2003@gmail.com>
2019-11-19 22:37:13 -0500

    Working to get install.packages to be recognized.

--- data-raw/make_internal_data.R
+++ data-raw/make_internal_data.R
@@ -7,8 +7,8 @@ hazard_material <- tibble::tribble(
     ~pattern,                           ~color,         ~emoji,
     "rm\\(ls = ls\\(\\)\\)",            crayon::red,    FIRE_EMOJI,
     "setwd\\([^\\)]+\\)",               crayon::red,    FIRE_EMOJI,
-    "system(2)?\\('[^\\')]+'\\)",       crayon::yellow, BELL_EMOJI,
-    "install.packages\\('[^\\')]+'\\)", crayon::yellow, BELL_EMOJI
+    "system(2)?\\((\"|')[^\\'\"]+(\"|')\\)",       crayon::yellow, BELL_EMOJI,
+    "install.packages\\((\"|')[^\\'\\\"]+(\"|')\\)", crayon::yellow, BELL_EMOJI
 )
 
 usethis::use_data(FIRE_EMOJI, BELL_EMOJI, hazard_material,
