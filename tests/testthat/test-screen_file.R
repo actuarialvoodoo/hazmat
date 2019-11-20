8740f84c8ec8fe6c7ad1898813cf66656424d2b5
Ryan Thomas <rthomas2003@gmail.com>
2019-11-19 21:45:55 -0500

    Adding tests for missing file and for folder

--- tests/testthat/test-screen_file.R
+++ tests/testthat/test-screen_file.R
@@ -1,5 +1,22 @@
-test_that("multiplication works", {
+test_that("scan of file with hazards works", {
     expect_silent(res <- screen_file("testdata/test_file.R", quiet = TRUE))
+    expect_s3_class(res, "tbl_df")
+    expect_equal(nrow(res), 2)
+})
+
+test_that("screen of file with no hazards works", {
+    expect_silent(res <- screen_file("testdata/test_file_case.r", quiet = TRUE))
+    expect_s3_class(res, "tbl_df")
+    expect_equal(nrow(res), 0)
+})
+
+test_that("missing file fails with expected error", {
+    expect_error(screen_file(), "valid file path")
+})
+
+
+test_that("screen of folder works", {
+    expect_silent(res <- screen_folder("testdata", quiet = TRUE))
     expect_s3_class(res, "tbl_df")
     expect_equal(nrow(res), 2)
 })
